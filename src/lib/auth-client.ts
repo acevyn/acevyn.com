f330cb1170adc146055962509acf487d59b8f1c3
Acevyn s he/him <acevyns@gmail.com>
2025-12-20 09:53:21 +0400

    Update auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000"
+  baseURL: process.env.BETTER_AUTH_URL || "http://acevyn-com.vercel.app"
 });
 
 export const { signIn, signUp, useSession } = authClient;
